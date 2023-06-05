8c7724b4fa2b1a1f8c5ee43fe2a5069d05c91f40
ramasakti <ramasakti1337@gmail.com>
2023-06-05 21:30:33 +0700

    Multer Upload Image

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
+const multer = require('multer');
 const path = require('path');
 const session = require('express-session');
 const db = require('./config')
 const bodyParser = require('body-parser');
 const flash = require('express-flash');
-const knex = require('knex');
 
+//Setup View Engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+//Access to Directory 'upload' as a public directory
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/upload', express.static(path.join(__dirname, 'upload')));
+
+//Setup Session
 app.use(
     session({
         secret: 'rahasimen', // Ganti dengan secret key yang lebih aman
@@ -16,12 +23,37 @@ app.use(
         saveUninitialized: true,
     })
 );
+
+//Setup Flash Message
 app.use(flash());
+
+//Setup Body Parser for Request
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//Login
+//Setup Multer Upload Image
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'upload'); // Menentukan direktori penyimpanan file
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname); // Menentukan nama file
+    }
+});
+
+const upload = multer({ storage: storage });
+
+//Start Router
+
+//Index
 app.get('/', (req, res) => {
+    res.render('index', {
+        
+    })
+});
+
+//Login
+app.get('/login', (req, res) => {
     const gagal = req.flash('fail')
     const berhasil = req.flash('success')
     res.render('login', {
@@ -31,7 +63,7 @@ app.get('/', (req, res) => {
 });
 
 //Proses Login
-app.post('/', (req, res) => {
+app.post('/login', (req, res) => {
     const { username, password } = req.body
     db('users')
         .select()
@@ -52,20 +84,28 @@ app.post('/', (req, res) => {
 
 //Dashboard
 app.get('/dashboard', (req, res) => {
-    const nama = req.session.nama
-    if (!nama) return res.redirect('/')
-    res.render('dashboard', {
-        nama
-    })
+    db('barang')
+        .select()
+        .then((barang) => {
+            const nama = req.session.nama
+            if (!nama) return res.redirect('/login')
+            res.render('dashboard', {
+                nama,
+                barang
+            })
+        })
+        .catch((err) => {
+            if (err) throw err
+        })
 });
 
 //Logout
 app.get('/logout', (req, res) => {
     req.session.destroy((error) => {
         if (error) {
-          console.log('Gagal menghapus session:', error);
+            console.log('Gagal menghapus session:', error);
         } else {
-          console.log('Session berhasil dihapus');
+            console.log('Session berhasil dihapus');
         }
         res.redirect('/');
     });
@@ -78,7 +118,7 @@ app.get('/register', (req, res) => {
 
 //Proses Register
 app.post('/register', (req, res) => {
-    const { username, password, email, nama } = req.body;
+    const { username, password, email, nama } = req.body
     const data = { username, password, email, nama }
 
     db('users')
@@ -94,6 +134,29 @@ app.post('/register', (req, res) => {
         })
 });
 
+//Store Barang
+app.post('/barang/store', upload.single('gambar'), (req, res) => {
+    const { nama_barang, harga, stok } = req.body
+    const gambar_barang = req.file.originalname
+    const dataBarang = { nama_barang, gambar_barang, harga, stok }
+    db('barang')
+        .insert(dataBarang)
+        .then(() => {
+            req.flash('success', `Berhasil menambahkan data barang!`)
+            res.redirect('/dashboard')
+        })
+        .catch((err) => {
+            if (err) throw err
+        })
+});
+
+//Put Barang
+
+//Delete Barang
+
+//End Router
+
+//Web Server
 app.listen(3000, () => {
     console.log('Server berjalan di http://localhost:3000');
 });
---
+++ migrations/20230605020506_create_barang_table.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('barang', function(table) {
+        table.increments('id_barang');
+        table.string('nama_barang').notNullable();
+        table.string('gambar_barang').notNullable();
+        table.integer('harga').notNullable();
+        table.integer('stok').notNullable();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('barang');
+};
